be48a321a16c486d0b352c274585c313d5c3c498
Damian Gasser <45496682+Attednev@users.noreply.github.com>
2023-09-03 16:12:32 +0200

    Fix chown flag

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@ echo "> Finished"
 echo "> Installing yay"
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -r $USER:$USER yay-git
+sudo chown -R $USER:$USER yay-git
 cd yay-git
 makepkg -si --noconfirm
 echo "> Finished"
